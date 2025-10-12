63d3e28bffd74aec07677ef04c8fb389817ee02e
Amit Roy <amit.akr.roy@gmail.com>
2025-10-12 22:41:04 +0200

    Added a modal to show movie info with rating and description

--- kino-prague/src/components/theatreData.js
+++ kino-prague/src/components/theatreData.js
@@ -8,7 +8,9 @@ export const THEATRE_DATA = [
         "movies": [
           {
             "name": "The Addams Family (1991)",
-            "time": "8:30 pm"
+            "time": "8:30 pm",
+            'rating' : '7.0',
+            'description' : 'The rich Addams family lives in a nonconformist manner. But this wealthy eccentricity is threatened when the family lawyer and a mother-son duo plot to steal the riches of the family. A battle ensues.'
           }
         ]
       },
